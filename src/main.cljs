933b109e1b536cb1cfdb5c693a137b45b14a368f
0xca551e <139658770+0xca551e@users.noreply.github.com>
2025-05-09 18:26:24 +0800

    Add three

--- src/main.cljs
+++ src/main.cljs
@@ -1,3 +1,36 @@
-(ns main)
+(ns main
+  (:require ["three" :as three]))
 
-(println "test")
+(def scene
+  (three/Scene.))
+
+(def camera
+  (three/PerspectiveCamera. 75 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 0.1 1000))
+
+(def renderer
+  (three/WebGLRenderer.))
+
+(.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
+
+(.appendChild (.-body js/document) (.-domElement renderer))
+
+(def geometry
+  (three/BoxGeometry. 1 1 1))
+
+(def material
+  (three/MeshBasicMaterial. { :color 0x00ff00 }))
+
+(def cube
+  (three/Mesh. geometry material))
+
+(.add scene cube)
+
+(set! (.-z (.-position camera)) 5)
+
+(println camera)
+
+(defn animate []
+  (set! (.-x (.-rotation cube)) (+ (.-x (.-rotation cube)) 0.01))
+  (set! (.-y (.-rotation cube)) (+ (.-y (.-rotation cube)) 0.01))
+  (.render renderer scene camera))
+(.setAnimationLoop renderer animate)
