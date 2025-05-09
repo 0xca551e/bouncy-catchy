2ef1f78ab0fad903640fa97d4570f1c1b254173e
0xca551e <139658770+0xca551e@users.noreply.github.com>
2025-05-09 19:55:31 +0800

    Add responsive canvas

--- src/main.cljs
+++ src/main.cljs
@@ -7,12 +7,14 @@
 (def camera
   (three/PerspectiveCamera. 75 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 0.1 1000))
 
+(def app-container
+  (.querySelector js/document "#app"))
+
+(def canvas
+  (.querySelector js/document "#canvas"))
+
 (def renderer
-  (three/WebGLRenderer.))
-
-(.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
-
-(.appendChild (.-body js/document) (.-domElement renderer))
+  (three/WebGLRenderer. {:canvas canvas}))
 
 (def geometry
   (three/BoxGeometry. 1 1 1))
@@ -27,9 +29,15 @@
 
 (set! (.-z (.-position camera)) 5)
 
-(println camera)
-
 (defn animate []
+  (let [width (.-clientWidth app-container)
+        height (.-clientHeight app-container)]
+    (when (or (not= (.-width canvas) width)
+              (not= (.-height canvas) height))
+      (.setSize renderer width height)
+      (set! (.-aspect camera) (/ width height))
+      (.updateProjectionMatrix camera)))
+
   (set! (.-x (.-rotation cube)) (+ (.-x (.-rotation cube)) 0.01))
   (set! (.-y (.-rotation cube)) (+ (.-y (.-rotation cube)) 0.01))
   (.render renderer scene camera))
