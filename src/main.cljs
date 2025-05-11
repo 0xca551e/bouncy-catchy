5e949085a7b2213cb67ebc9612506f70bb2cd0f0
0xca551e <139658770+0xca551e@users.noreply.github.com>
2025-05-11 13:41:12 +0800

    Reorganize code

--- src/main.cljs
+++ src/main.cljs
@@ -6,41 +6,99 @@
    ["three/examples/jsm/controls/TransformControls" :refer [TransformControls]]
    [input]))
 
+;; dom objects
 (def app-container
   (.querySelector js/document "#app"))
 
 (def canvas
   (.querySelector js/document "#canvas"))
 
-(def world
+;; game world
+(def ecs
+  (miniplex/World.))
+
+(def physics-engine
   (let [gravity {:x 0 :y -9.81 :z 0}]
     (rapier/World. gravity)))
 
+;; - setting up threejs world
+(def renderer
+  (let [r (three/WebGLRenderer. {:canvas main/canvas})]
+    (set! (.. r -shadowMap -enabled) true)
+    r))
+
 (def scene
-  (three/Scene.))
+  (let [s (three/Scene.)]
+    (set! (.-background s) (three/Color. 0xbfd1e5))
+    s))
 
 (def camera
-  (three/PerspectiveCamera. 75 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 0.1 1000))
-(set! (.-z (.-position camera)) 10)
-(set! (.-y (.-position camera)) 10)
-(.lookAt camera 0 0 0)
+  (let [c (three/PerspectiveCamera.
+           75
+           (/ (.-innerWidth js/window)
+              (.-innerHeight js/window))
+           0.1
+           1000)]
+    (set! (.-z (.-position c)) 10)
+    (set! (.-y (.-position c)) 10)
+    (.lookAt c 0 0 0)
+    c))
 
 (def control
-  (TransformControls. camera canvas))
-(.add scene (.getHelper control))
+  (let [c (TransformControls. camera main/canvas)]
+    (.add scene (.getHelper c))
+    c))
 
-(def mplexworld
-  (miniplex/World.))
+(let [ambient-light (three/HemisphereLight. 0x555555 0xffffff)]
+  (.add scene ambient-light))
+(let [sunlight (three/DirectionalLight. 0xffffff 4)]
+  (.set (.-position sunlight) -5 10 0)
+  (set! (.-castShadow sunlight) true)
+  (set! (.. sunlight -shadow -radius) 3)
+  (set! (.. sunlight -shadow -blurSamples) 8)
+  (set! (.. sunlight -shadow -mapSize -width) 1024)
+  (set! (.. sunlight -shadow -mapSize -height) 1024)
+  (let [size 10]
+    (set! (.. sunlight -shadow -camera -left) (- size))
+    (set! (.. sunlight -shadow -camera -bottom) (- size))
+    (set! (.. sunlight -shadow -camera -right) size)
+    (set! (.. sunlight -shadow -camera -top) size)
+    (set! (.. sunlight -shadow -camera -near) 1)
+    (set! (.. sunlight -shadow -camera -far) 50))
+  (.add scene sunlight))
 
-(def physics-query (.with mplexworld "mesh" "physics"))
-(def controllable-mesh-query (.with mplexworld "mesh" "controllable"))
-
+;; queries
+(def physics-query (.with ecs "mesh" "physics"))
 (.subscribe (.-onEntityAdded physics-query) (fn [e]
                                               (.add scene (:mesh e))))
 (.subscribe (.-onEntityRemoved physics-query) (fn [e]
                                                 (.remove scene (:mesh e))
                                                 (.removeRigidBody (:body (:physics e)))
                                                 (.removeCollider (:collider (:physics e)))))
+
+(def controllable-mesh-query (.with ecs "mesh" "controllable"))
+
+;; systems
+(defn resize-renderer-to-display-size []
+  (let [width (.-clientWidth main/app-container)
+        height (.-clientHeight main/app-container)]
+    (when (or (not= (.-width main/canvas) width)
+              (not= (.-height main/canvas) height))
+      (.setSize renderer width height)
+      (set! (.-aspect camera) (/ width height))
+      (.updateProjectionMatrix camera))))
+
+(defn step-physics []
+  (.step physics-engine))
+
+(defn handle-object-selection []
+  (.setFromCamera input/raycaster input/pointer camera)
+  (when (input/just-mouse-up-nodrag 0)
+    (let* [intersects (.intersectObjects input/raycaster (.-children scene) false)
+           first-hit (nth intersects 0)]
+          (if first-hit
+            (.attach control (.-object first-hit))
+            nil))))
 
 (defn sync-mesh-to-physics []
   (doseq [{mesh :mesh {body :body} :physics} physics-query]
@@ -53,91 +111,67 @@
         (.setRotation body r))
       (let [t (.translation body)
             r (.rotation body)]
-        (.set (.-position mesh) (:x t) (:y t) (:z t))
-        (.set (.-quaternion mesh) (:x r) (:y r) (:z r) (:w r))))))
+        (.set (.-position mesh)
+              (:x t)
+              (:y t)
+              (:z t))
+        (.set (.-quaternion mesh)
+              (:x r)
+              (:y r)
+              (:z r)
+              (:w r))))))
 
-(defn assemble-physics-cube []
-  (let* [geometry (three/BoxGeometry. 1 1 1)
+(defn render []
+  (.render renderer scene camera))
+
+(defn animation-frame []
+  (resize-renderer-to-display-size)
+  (step-physics)
+  (sync-mesh-to-physics)
+  (handle-object-selection)
+  (render)
+  (input/post-update))
+
+;; assemblages
+(def ball-radius 0.5)
+(defn ^:export assemble-physics-ball [position velocity]
+  (let* [;geometry (three/BoxGeometry. 1 1 1)
+         geometry (three/SphereGeometry. ball-radius 16 8)
          material (three/MeshStandardMaterial. {:color 0x00ff00})
          mesh (three/Mesh. geometry material)
          rigid-body-desc (-> (.dynamic rapier/RigidBodyDesc)
-                             (.setTranslation 0 8 0)
-                             (.setRotation {:x 0.4254518 :y -0.237339 :z 0.4254518 :w 0.762661}))
-         rigid-body (.createRigidBody world rigid-body-desc)
-         collider-desc (.cuboid rapier/ColliderDesc 1 1 1)
-         collider (.createCollider world collider-desc rigid-body)]
+                             (.setTranslation (:x position)
+                                              (:y position)
+                                              (:z position))
+                             (.setLinvel (:x velocity)
+                                         (:y velocity)
+                                         (:z velocity)))
+         rigid-body (.createRigidBody physics-engine rigid-body-desc)
+         ;; collider-desc (.cuboid rapier/ColliderDesc 1 1 1)
+         collider-desc (.ball rapier/ColliderDesc ball-radius)
+         collider (.createCollider physics-engine collider-desc rigid-body)]
         (set! (.-castShadow mesh) true)
         {:mesh mesh
          :physics {:body rigid-body :collider collider}}))
 
-(set! (.-background scene) (three/Color. 0xbfd1e5))
+;; main
+(defn start []
+  (input/init)
 
-(def ambient
-  (three/HemisphereLight. 0x555555 0xffffff))
+  (let [cube (assemble-physics-ball (three/Vector3. 0 10 0) (three/Vector3.))]
+    (.add ecs cube))
 
-(.add scene ambient)
+   ;; - set up three ground
+   ;; TODO: make assemblage
+  (let* [geometry (three/BoxGeometry. 10 1 10)
+         material (three/MeshStandardMaterial. {:color 0xffffff})
+         ground (three/Mesh. geometry material)]
+    (set! (.. ground -position -y) 1)
+    (set! (.-receiveShadow ground) true)
+    (.add scene ground))
+  (let [ground-body (rapier/ColliderDesc.cuboid 10 1 10)]
+    (.createCollider physics-engine ground-body))
 
-(def light
-  (three/DirectionalLight. 0xffffff 4))
+  (.setAnimationLoop renderer animation-frame))
 
-(.set (.-position light) -5 10 0)
-(set! (.-castShadow light) true)
-(set! (.. light -shadow -radius) 3)
-(set! (.. light -shadow -blurSamples) 8)
-(set! (.. light -shadow -mapSize -width) 1024)
-(set! (.. light -shadow -mapSize -height) 1024)
-(let [size 10]
-  (set! (.. light -shadow -camera -left) (- size))
-  (set! (.. light -shadow -camera -bottom) (- size))
-  (set! (.. light -shadow -camera -right) size)
-  (set! (.. light -shadow -camera -top) size)
-  (set! (.. light -shadow -camera -near) 1)
-  (set! (.. light -shadow -camera -far) 50))
-(.add scene light)
-
-(def ground
-  (let [geometry (three/BoxGeometry. 10 1 10)
-        material (three/MeshStandardMaterial. { :color 0xffffff })]
-    (three/Mesh. geometry material)))
-(let [ground-body (rapier/ColliderDesc.cuboid 10 1 10)]
-  (.createCollider world ground-body))
-
-;; TODO: ground physics object is off by 1?
-(set! (.. ground -position -y) 1)
-
-(set! (.-receiveShadow ground) true)
-(.add scene ground)
-
-(def renderer
-  (three/WebGLRenderer. {:canvas canvas}))
-
-(set! (.. renderer -shadowMap -enabled) true)
-
-(let [cube (assemble-physics-cube)]
-  (.add mplexworld cube))
-
-(input/init)
-
-(defn animate []
-  (let [width (.-clientWidth app-container)
-        height (.-clientHeight app-container)]
-    (when (or (not= (.-width canvas) width)
-              (not= (.-height canvas) height))
-      (.setSize renderer width height)
-      (set! (.-aspect camera) (/ width height))
-      (.updateProjectionMatrix camera)))
-  (.step world)
-  (sync-mesh-to-physics)
-
-  (.setFromCamera input/raycaster input/pointer camera)
-  (when (input/just-mouse-up-nodrag 0)
-    (let* [intersects (.intersectObjects input/raycaster (.-children scene) false)
-           first-hit (nth intersects 0)]
-      (if first-hit
-        (.attach control (.-object first-hit))
-        nil)))
-
-  (.render renderer scene camera)
-
-  (input/post-update))
-(.setAnimationLoop renderer animate)
+(start)
