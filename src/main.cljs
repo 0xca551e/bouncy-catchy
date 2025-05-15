d384caff720f2a7fb786313c9bb80a7531f93410
0xca551e <139658770+0xca551e@users.noreply.github.com>
2025-05-15 10:16:06 +0800

    Fix my timestep

--- src/main.cljs
+++ src/main.cljs
@@ -12,17 +12,22 @@
 
 (def game (ecs/make))
 
+(def accumulator 0)
+(def timestep-ms 16.666)
 (def last-time 0)
 (defn animation-frame [time]
   (let [delta (- time last-time)]
-    (renderer/resize-to-display-size game)
-    (physics/step-physics game)
-    (timerbar/update-timerbar-entity game delta)
-    (physics/sync-mesh-to-physics game)
-    (wall/handle-object-selection game)
-    (wall/handle-collision game)
-    (renderer/render game)
-    (input/post-update game))
+    (set! accumulator (+ accumulator delta))
+    (when (> accumulator timestep-ms)
+      (set! accumulator (mod accumulator timestep-ms))
+      (renderer/resize-to-display-size game)
+      (physics/step-physics game)
+      (timerbar/update-timerbar-entity game timestep-ms)
+      (physics/sync-mesh-to-physics game)
+      (wall/handle-object-selection game)
+      (wall/handle-collision game)
+      (renderer/render game)
+      (input/post-update game)))
   (set! last-time time))
 
 (defn ^:async start []
